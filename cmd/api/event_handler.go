5511d91cd3d847b8c9afd4d57c617ee9a1fcf4f4
momomo0206 <momomo0206@gmail.com>
2025-11-25 22:00:18 +0900

    04: auth handler implementation

--- cmd/api/event_handler.go
+++ cmd/api/event_handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -12,12 +13,14 @@ func (app *application) createEvent(c *gin.Context) {
 	var event database.Event
 
 	if err := c.ShouldBindJSON(&event); err != nil {
+		fmt.Println(err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	err := app.models.Events.Insert(&event)
 	if err != nil {
+		fmt.Println(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create event"})
 		return
 	}
